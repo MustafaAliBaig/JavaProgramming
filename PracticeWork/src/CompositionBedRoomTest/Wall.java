3d381d6374fb3215dc0cb6b8d6e281f4f5efa145
MIRZA <mz564k@att.com>
2020-04-29 02:29:44 -0700

    Add existing file

---
+++ PracticeWork/src/CompositionBedRoomTest/Wall.java
@@ -0,0 +1,16 @@
+package CompositionBedRoomTest;
+
+public class Wall {
+	private String direction;
+	
+	public Wall(String direction)
+	{
+		this.direction=direction;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+	
+
+}
