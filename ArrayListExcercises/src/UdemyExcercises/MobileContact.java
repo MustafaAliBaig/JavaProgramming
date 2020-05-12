e46c917a4e792174356761ab8462b0be155076c3
mz564k <mz564k@CACDTL14JP925A.ITServices.sbc.com>
2020-05-12 04:26:19 -0700

    05/12/2020-Challenge Excercise-1

--- ArrayListExcercises/src/UdemyExcercises/MobileContact.java
+++ ArrayListExcercises/src/UdemyExcercises/MobileContact.java
@@ -5,5 +5,17 @@ public class MobileContact {
 	private String name;
 	private int phno;
 	
+	public MobileContact(String name, int phno) {
+		this.name = name;
+		this.phno = phno;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public int getPhno() {
+		return phno;
+	}
+	
 }
---
+++ ArrayListExcercises/src/UdemyExcercises/MobilePhone.java
@@ -0,0 +1,50 @@
+package UdemyExcercises;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+
+	private String myNumber;
+	private	ArrayList<MobileContact> myContacts;
+	public MobilePhone(String myNumber) {
+		this.myNumber = myNumber;
+		this.myContacts=new ArrayList<MobileContact>();
+	}
+	
+	public boolean  addNewContact(MobileContact contact) {
+		if(findContact(contact.getName())>=0) {
+			System.out.println("Contact is already on file");
+		return false;
+		}
+		myContacts.add(contact);
+		return true;
+	}
+	
+	public boolean updateContact(MobileContact oldContact,MobileContact newContact) {
+		int foundPosition=findContact(oldContact);
+		if(foundPosition<0)
+		{
+			System.out.println(oldContact.getName() +", was not found.");
+		return false;
+		}
+		this.myContacts.set(foundPosition, newContact);
+		System.out.println(oldContact.getName()+", Was replaced with "+newContact.getName());
+		return true;
+	}
+	private int findContact(MobileContact contact)
+	{
+		return this.myContacts.indexOf(contact);
+	}
+		private int findContact(String contactName)
+		{
+			for(int i=0;i<this.myContacts.size();i++)
+			{
+				MobileContact contact=this.myContacts.get(i);
+				if(contact.getName().equals(contactName)) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+}
