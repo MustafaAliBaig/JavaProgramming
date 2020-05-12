e0d8d1c529b0368a1fe207fc6f4e7ba8640eb352
mz564k <mz564k@CACDTL14JP925A.ITServices.sbc.com>
2020-05-12 03:30:43 -0700

    "05/12/2020 Commit"

---
+++ ArrayListExcercises/src/UdemyExcercises/MobileContact.java
@@ -0,0 +1,9 @@
+package UdemyExcercises;
+
+public class MobileContact {
+	
+	private String name;
+	private int phno;
+	
+
+}
---
+++ ArrayListExcercises/src/UdemyExcercises/MobilePhoneMain.java
@@ -0,0 +1,14 @@
+package UdemyExcercises;
+
+import java.util.ArrayList;
+
+public class MobilePhoneMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<MobileContact> mobilephone=new ArrayList<MobileContact>();
+		
+
+	}
+
+}
