c9aff895268f7c1b443ec8fd8def1a8375a7307c
mz564k <mz564k@CACDTL14JP925A.ITServices.sbc.com>
2020-05-23 14:28:41 -0700

    Added Description of problem

---
+++ ArrayAutoBoxing-Unboxing/src/part1/ArrayBoxingUnboxingMain.java
@@ -0,0 +1,32 @@
+package part1;
+
+public class ArrayBoxingUnboxingMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Your job is to create a simple banking application
+		 *There should be a bank Class
+		 *It should have an arrayList of Branches
+		 *Each Branch should have an ArrayList o Customers
+		 *The Customer class should have an  arrayList of Doubles(Transactions)
+		 *Customer:
+		 *Name, and the ArrayLisy of Doubles.
+		 *Branch:
+		 *Need to be able to add a new customer and initial transaction amount
+		 *Also needs to add additional Transactions for that customer/Branch
+		 *Bank:
+		 *Add a new branch
+		 *Add a customer to that branch with initial transaction
+		 *Add a transaction for an existing customer for that branch
+		 *Show a list of customers for a particular branch and optionally a list
+		 *of their transactions
+		 *Demonstrate autoboxing and unboxing in your code
+		 *HInt: Transactions
+		 *Add data Validation.
+		 *Eg check if exists, or does not exists, etc.
+		 *Think about where you are adding the code to perform certain actions
+		 **/
+
+	}
+
+}
